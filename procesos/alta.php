bae2db78c69992a6998dc709386bdf05bd9c9ce6
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-10-19 12:55:26 +0200

    Commit 3

--- procesos/alta.php
+++ procesos/alta.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
+<?php
+    //Juan Manuel Toscano Reyes
+    require 'config_bd.php'; //Llamo al archivo donde están las constantes
+    $conexion=mysqli_connect(SERVIDORBD,USUARIO,CONTRASENIA,BASEDATOS); //Realizo la conexión con la base de datos
+?>
 <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <title>BD_Empleados</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Alta</title>
     </head>
     <body>
+        <h1>Dar de alta</h1>
+        <form name="alta" action="#" method="POST">
+            DNI: <input type="text" name="dni" /><br />
+            Nombre: <input type="text" name="nombre" /><br />
+            Correo: <input type="text" name="correo" /><br />
+            Teléfono: <input type="text" name="telefono" /><br /><br />
+            <input type="submit" value="Enviar" name="enviar" />
+            <input type="reset" value="Borrar" />
+        
+        </form>
         <?php
-            require 'config_bd.php'; //Llamo al archivo donde están las constantes
-            $conexion=mysqli_connect(SERVIDORBD,USUARIO,CONTRASENIA,BASEDATOS); //Realizo la conexión con la base de datos
-            echo '<h1>Dar de alta</h1>';
-            if(isset($_POST['enviar']){
-                echo 
-                '
-                    <form name="alta" action="#" method="POST">
-                        DNI: <input type="text" name="dni" /><br />
-                        Nombre: <input type="text" name="nombre" /><br />
-                        Correo: <input type="text" name="correo" /><br />
-                        Teléfono: <input type="text" name="telefono" /><br /><br />
-                        <input type="submit" value="Enviar" name="enviar" />
-                        <input type="reset" value="Borrar" />
-                    
-                    </form>
-                ';
-            }else{
+            if(isset($_POST['enviar'])){
                 $consulta='INSERT INTO empleados () VALUES ("","$_POST['dni']","$_POST['nombre']","$_POST['correo']","$_POST['telefono']");';
                 try{
                     $resultado=mysqli_query($conexion,$consulta);
@@ -40,6 +41,9 @@
                     echo 'Excepción capturada';
                 }
             }
+            
+            
+        
             
         ?>
     </body>
