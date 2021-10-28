dfc2ec4da6372387bca2861ce640ff9ca0d0de09
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-10-28 10:11:57 +0200

    Commit 9

--- procesos/alta.php
+++ procesos/alta.php
@@ -10,19 +10,41 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Juan Manuel Toscano Reyes">
+        <link rel="stylesheet" href="../css/index.css">
+        <link rel="stylesheet" href="../css/alta.css">
         <title>Alta</title>
     </head>
     <body>
-        <h1>Dar de alta</h1>
-        <form name="alta" action="#" method="POST">
-            DNI: <input type="text" name="dni" /><br />
-            Nombre: <input type="text" name="nombre" /><br />
-            Correo: <input type="text" name="correo" /><br />
-            Teléfono: <input type="text" name="telefono" /><br /><br />
-            <input type="submit" value="Enviar" name="enviar" />
-            <input type="reset" value="Borrar" />
-        </form>
-        <a href="../index.html">Volver al índice</a><br />
+        <header>
+            <h1>Dar de alta</h1>
+        </header>
+        <nav id="horizontal">
+            <ul>
+                <li><a href="../index.html">Índice</a></li>
+            </ul>
+        </nav>
+        <aside>
+            <nav id="vertical">
+                <ul>
+                    <li><a href="procesos/alta.php">Dar de Alta</a></li>
+                    <li><a href="procesos/listado.php">Listado Empleados</a></li>
+                </ul>
+            </nav>
+            <section>
+                <form name="alta" action="#" method="POST">
+                    DNI: <input type="text" name="dni" /><br />
+                    Nombre: <input type="text" name="nombre" /><br />
+                    Correo: <input type="text" name="correo" /><br />
+                    Teléfono: <input type="text" name="telefono" /><br /><br />
+                    <input type="submit" value="Enviar" name="enviar" />
+                    <input type="reset" value="Borrar" />
+                </form>
+                <a href="../index.html">Volver al índice</a><br />
+            </section>
+        </aside>
+        <footer>
+            <h3>Footer</h3>
+        </footer>
     </body>
 </html>
 <?php
